47e7625d9f67c3e811090025377912645985bc7e
vanessa <vanessa.wagner@students.coding-bootcamps.eu>
2021-05-07 21:36:10 +0200

    add new todo to list

--- script.js
+++ script.js
@@ -4,7 +4,34 @@
 2. List all added todos
 3. Added todos can be marked as done 
 
-1. Step
-- Eingabezeile
-- add button
+2. Step
+- create list
+- create listItem by click-event
+- create array
+- add listItem content to array
 */
+
+// array of added todos
+const listContent = [];
+
+// click-event add todo
+const addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", createTodoItem);
+
+// create todoItem
+function createTodoItem() {
+  // get input-text of currentTodo
+  const currentTodo = document.getElementById("input-text").value;
+
+  // select todoList for adding new todo
+  const todoList = document.getElementById("todoList");
+
+  // create new listItem
+  const listItem = document.createElement("li");
+
+  // add text to listItem
+  listItem.appendChild(document.createTextNode(currentTodo));
+
+  // add listElement to list
+  todoList.appendChild(listItem);
+}
