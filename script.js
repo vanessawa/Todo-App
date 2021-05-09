8048b76d5dd78363331acac1e678fba385d10e37
vanessa <vanessa.wagner@students.coding-bootcamps.eu>
2021-05-09 23:35:30 +0200

    select todos

--- script.js
+++ script.js
@@ -5,14 +5,11 @@
 2. Add a button which will delete all done todos
 
 1. Step
-- div with "all todos", 
-  "open todos" or "done todos"
-- checkbox to all 
 - click-event for each
 */
 
 // array of added todos
-const listContent = [];
+const allTodos = [];
 
 // click-event add todo
 const addBtn = document.querySelector("#addBtn");
@@ -35,7 +32,41 @@ function createTodoItem() {
   // add listElement to list
   todoList.appendChild(listItem);
 
+  // add checkbox to listItem
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
   listItem.appendChild(checkbox);
+
+  // add items to array
+  allTodos.push(listItem);
+  allTodos.push(checkbox);
+}
+
+// click-event select todos
+//const allTodosBtn = document.querySelector("#allTodos");
+//allTodosBtn.addEventListener("click", showAllTodos);
+
+const openTodosBtn = document.querySelector("#openTodos");
+openTodosBtn.addEventListener("click", showOpenTodos);
+
+const doneTodosBtn = document.querySelector("#doneTodos");
+doneTodosBtn.addEventListener("click", showDoneTodos);
+
+function showOpenTodos() {
+  for (currentTodo of allTodos) {
+    if (currentTodo.checked == true) {
+      currentTodo.parentElement.style.visibility = "hidden";
+    } else {
+      currentTodo.parentElement.style.visibility = "visible";
+    }
+  }
+}
+function showDoneTodos() {
+  for (currentTodo of allTodos) {
+    if (currentTodo.checked == false) {
+      currentTodo.parentElement.style.visibility = "hidden";
+    } else {
+      currentTodo.parentElement.style.visibility = "visible";
+    }
+  }
 }
